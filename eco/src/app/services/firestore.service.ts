90491841fca12359d79534648d1d9e33ded54748
Anthony Perez <anperezcue@uide.edu.ec>
2025-08-06 19:07:35 -0500

    Marketplace Boceto1

---
+++ eco/src/app/services/firestore.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Firestore, collection, collectionData, addDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { MarketplaceItem } from '../components/marketplace/marketplace';
+
+@Injectable({ providedIn: 'root' })
+export class FirestoreService {
+  constructor(private firestore: Firestore) {}
+
+  getItems(): Observable<MarketplaceItem[]> {
+    const itemsRef = collection(this.firestore, 'productos');
+    return collectionData(itemsRef, { idField: 'id' }) as Observable<MarketplaceItem[]>;
+  }
+
+  addItem(item: Omit<MarketplaceItem, 'id'>): Promise<void> {
+    const itemsRef = collection(this.firestore, 'productos');
+    return addDoc(itemsRef, item).then(() => {});
+  }
+}
